21bd8c7056664ffcb75c5de734575320efd9165f
thamycoelho <thamycoelho10@gmail.com>
2023-02-24 18:12:28 -0300

    fix resume training from checkpoint

--- training.py
+++ training.py
@@ -51,7 +51,7 @@ class Trainer:
                 checkpoint_paths = [self.output_dir / 'checkpoint.pth', self.output_dir / 'best.pth']
                 for checkpoint_path in checkpoint_paths:
                     state_dict = {
-                        'model': self.model.state_dict(),
+                        'model': self.model.module.state_dict(),
                         'optimizer': self.optimizer.state_dict(),
                         'lr_scheduler': self.lr_scheduler.state_dict(),
                         'epoch': epoch,

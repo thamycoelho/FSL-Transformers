226dac40528fce635730a67b35e930a5ceaf4d5f
thamycoelho <thamycoelho10@gmail.com>
2023-04-05 17:18:32 -0300

    change  cofusion matrix plot

--- utils/metrics.py
+++ utils/metrics.py
@@ -7,50 +7,22 @@ import collections
 import scipy 
 import os
 import random
+import seaborn as sns
 
-
-def _save_confusion_matrix(cm,
-                      target_names,
+def _save_confusion_matrix(cm, target_names,
                       save_path,
-                      title='Confusion matrix',
-                      cmap=None,
-                      normalize=True):
+                      title='Confusion matrix'):
     accuracy = np.trace(cm) / float(np.sum(cm))
     misclass = 1 - accuracy
 
-    if cmap is None:
-        cmap = plt.get_cmap('Blues')
-
+    # normalize
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    
     size = cm.shape[0]
-    plt.figure(figsize=(size, size))
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-
-    if target_names is not None:
-        tick_marks = np.arange(len(target_names))
-        plt.xticks(tick_marks, target_names, rotation=45)
-        plt.yticks(tick_marks, target_names)
-
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-
-    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        if normalize:
-            plt.text(j, i, "{:0.2f}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-        else:
-            plt.text(j, i, "{:,}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    fig, ax = plt.subplots(figsize=(size + 5,size + 2))
+    sns.heatmap(cm, annot=True, fmt='.2f', xticklabels=target_names, yticklabels=target_names, cmap='Blues')
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
     plt.savefig(save_path / "confusion_matrix.png")
 
 def generate_confusion_matrix(y_true, y_pred, label_names, path):

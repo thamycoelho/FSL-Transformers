9e47e3dcd7f48f50d0b08a78e855a8bd677f43a2
thamycoelho <thamycoelho10@gmail.com>
2023-04-20 16:52:48 -0300

    script to run experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,9 +3,12 @@ device=$1
 
 for backbone in deit dino resnet50 resnet50_dino
 do
+echo "experiments for $backbone" 
 # train and eval deit
 CUDA_VISIBLE_DEVICES=$device python3 main.py --output output/ --dataset places_600 --backbone $backbone --experiment_name $backbone
 CUDA_VISIBLE_DEVICES=$device python3 main.py --output output/ --dataset places_600 --backbone $backbone --experiment_name $backbone --eval
+mv output/places_600/$backbone/confusion_matrix.png output/places_600/$backbone/confusion_matrix_no_finetuning.png
 CUDA_VISIBLE_DEVICES=$device python3 main.py --output output/ --dataset places_600 --backbone $backbone --experiment_name $backbone --eval --resume output/places_600/$backbone/best.pth
+mv output/places_600/$backbone/confusion_matrix.png output/places_600/$backbone/confusion_matrix_finetuning.png
 done
 

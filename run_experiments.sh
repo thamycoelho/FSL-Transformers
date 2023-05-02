eeab5bf9d1ee5efe7e8d853f1f9e73d7f2e6733c
thamycoelho <thamycoelho10@gmail.com>
2023-05-02 11:27:22 -0300

    updating experiment script

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 device=$1
 
-for backbone in deit dino resnet50 resnet50_dino
+for((i=2; i <= $#; i++))
 do
+backbone=${!i}
 echo "experiments for $backbone" 
 # train and eval deit
 CUDA_VISIBLE_DEVICES=$device python3 main.py --output output/ --dataset places_600 --backbone $backbone --experiment_name $backbone
@@ -11,4 +12,3 @@ mv output/places_600/$backbone/confusion_matrix.png output/places_600/$backbone/
 CUDA_VISIBLE_DEVICES=$device python3 main.py --output output/ --dataset places_600 --backbone $backbone --experiment_name $backbone --eval --resume output/places_600/$backbone/best.pth
 mv output/places_600/$backbone/confusion_matrix.png output/places_600/$backbone/confusion_matrix_finetuning.png
 done
-
